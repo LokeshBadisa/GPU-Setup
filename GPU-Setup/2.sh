d56914cc55f5e658729b7f8a209b18d889dbd3fa
LokeshBadisa <102676788+LokeshBadisa@users.noreply.github.com>
2022-12-15 16:11:29 +0530

    Update 2.sh

--- GPU-Setup/2.sh
+++ GPU-Setup/2.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo apt install make 
-sudo apt install gcc
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
 sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-ubuntu2204-11-7-local_11.7.0-515.43.04-1_amd64.deb
