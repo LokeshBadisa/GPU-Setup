d2514531ab1dfd09fe8ecb163663ccb801db88ee
LokeshBadisa <102676788+LokeshBadisa@users.noreply.github.com>
2022-12-15 16:37:29 +0530

    Update 3.sh

--- GPU-Setup/3.sh
+++ GPU-Setup/3.sh
@@ -1,4 +1,3 @@
-mv (rename) cudnn
 sudo cp cudnn/include/cudnn*.h /usr/local/cuda-11.7/include
 sudo cp -P cudnn/lib/libcudnn* /usr/local/cuda-11.7/lib64
 sudo chmod a+r /usr/local/cuda-11.7/include/cudnn*.h /usr/local/cuda-11.7/lib64/libcudnn*
