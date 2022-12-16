43a52823a0785fa2ad802f00f3ac6aa067048fa9
LokeshBadisa <102676788+LokeshBadisa@users.noreply.github.com>
2022-12-16 12:26:01 +0530

    Update 4.sh

--- GPU-Setup/4.sh
+++ GPU-Setup/4.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
+sh Anaconda3-2022.10-Linux-x86_64.sh
+export PATH=~/anaconda3/bin:$PATH
+conda init
 conda config --set auto_activate_base false
-
